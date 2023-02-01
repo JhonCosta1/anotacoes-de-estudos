27d171a926c092e548f476cad68b13e4a89c81c5
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-01 15:32:09 -0300

    Conjuntos
    Conjuntos - não aceita repetição de dados, sem index

--- anotacao2.js
+++ anotacao2.js
@@ -1,4 +1,4 @@
-
+//Rest/Spread ...
 function total(...precos){
     let total = 0;
     precos.forEach(p => total += p);
@@ -17,3 +17,22 @@ let frutas1 = ['manga', 'pera'];
 let frutas2 = ['jaca', 'uva'];
 
 let compras = [...frutas1, ...frutas2];
+
+//===========================================
+
+//Conjutos - não aceita repetição de dados, sem index
+
+let cursos = new Set();
+
+cursos.add('Programação em C').add('Programação em Java');
+cursos.add('Banco de Dados');
+
+//tamanho do conjunto
+console.log(cursos.size);
+
+//verificar se está no conjunto true ou false 
+
+console.log(cursos.has('Banco de dados'));
+
+//deletar elemento de conjunto
+cursos.delete('Programação em C');
