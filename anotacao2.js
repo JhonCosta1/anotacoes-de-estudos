b3d8e90b3bb316c0d6ae1a4784d92ee09dcc902c
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-01 15:05:35 -0300

    Rest/Spread
    ... - espalhar ou juntar valores

---
+++ anotacao2.js
@@ -0,0 +1,19 @@
+
+function total(...precos){
+    let total = 0;
+    precos.forEach(p => total += p);
+    return total
+}
+
+console.log(total(23.44, 22, 3.45, 5.67).toFixed(2));
+
+console.log(total(23, 67));
+
+console.log(total(3, 6, 77));
+
+console.log(total());
+
+let frutas1 = ['manga', 'pera'];
+let frutas2 = ['jaca', 'uva'];
+
+let compras = [...frutas1, ...frutas2];
