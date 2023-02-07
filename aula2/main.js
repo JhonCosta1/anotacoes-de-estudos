58387de5fdbaab39fd6f1e85d2f68b92d602912e
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-07 20:00:35 -0300

    Exemplo
    Exemplo te tipos de dados

--- aula2/main.js
+++ aula2/main.js
@@ -3,6 +3,17 @@
 //Primitivo especial: Null
 //Complexos: objeto e função
 
+
+//String - texto puro
+//Number - número inteiro ou flutuante
+//Boolean - True ou falso
+//Symbol - Valor exclusivo e imutável
+//BigInt - número maiores que 2*5-3
+//Undefined - valor não atribuido, espaço sem nada
+
+
+
+
 let estudar = true
 
 console.log(estudar);
