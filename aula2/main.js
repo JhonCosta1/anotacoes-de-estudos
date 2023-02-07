53b3f68a590cf46feee9a8c5ae5fa3b05a0806ce
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-07 19:57:18 -0300

    Estudo tipo de tados
    Tipos de dados JS

---
+++ aula2/main.js
@@ -0,0 +1,8 @@
+// TIPOS DE DADOS
+//Primitivos: string, number, BigInt, boolean, undefined e symbol
+//Primitivo especial: Null
+//Complexos: objeto e função
+
+let estudar = true
+
+console.log(estudar);
