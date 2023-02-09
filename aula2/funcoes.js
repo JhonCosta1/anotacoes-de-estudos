55dc8ee66de70e9cb9cdf2ccbe9b91e75a1ab440
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-09 19:18:53 -0300

    Função media e let = função
    atribuindo uma função a uma variável

--- aula2/funcoes.js
+++ aula2/funcoes.js
@@ -4,3 +4,16 @@ function calcularArea(lado){
     let area = lado * lado;
     return area;
 }
+
+console.log(calcularArea(10))
+
+function calcularMedia(num1, num2, num3){
+
+    let media = (num1 + num2 + num3) / 3;
+    return media;
+
+}
+
+let mediaFinal = calcularMedia;
+
+console.log(mediaFinal(3, 3, 3))
